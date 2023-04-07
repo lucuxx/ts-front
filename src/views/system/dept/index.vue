231a339fc3c0eee88a469b628c999977a61124c0
lucux <381274175@qq.com>
2023-04-07 23:49:51 +0800

    fix: 测试element-ui 补丁

--- src/views/system/dept/index.vue
+++ src/views/system/dept/index.vue
@@ -146,7 +146,7 @@
     </el-table>
 
     <!-- 添加或修改机构对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="600px" append-to-body>
+    <el-dialog :title="title" :visible.sync="open" width="600px" :fullscreen="true" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-row>
           <el-col :span="24" v-if="form.parentId !== 0">
